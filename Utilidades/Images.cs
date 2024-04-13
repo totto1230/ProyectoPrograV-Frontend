28634a6ca54b0053c718a5be16125ad2172a60a7
totto1230 <joseph.granados@hotmail.com>
2024-04-13 16:21:13 -0600

    Image URL update

--- Utilidades/Images.cs
+++ Utilidades/Images.cs
@@ -9,21 +9,21 @@ namespace Login1.Utilidades
     public class Images
     {
         public static string[] url = {
-                "https://storage.googleapis.com/proyectoprogra/1.jpg",
-                "https://storage.googleapis.com/proyectoprogra/2.jpg",
-                "https://storage.googleapis.com/proyectoprogra/3.jpg",
-                "https://storage.googleapis.com/proyectoprogra/4.jpg",
-                "https://storage.googleapis.com/proyectoprogra/5.jpg",
-                "https://storage.googleapis.com/proyectoprogra/6.jpg",
-                "https://storage.googleapis.com/proyectoprogra/7.jpg",
-                "https://storage.googleapis.com/proyectoprogra/8.jpg",
-                "https://storage.googleapis.com/proyectoprogra/9.jpg",
-                "https://storage.googleapis.com/proyectoprogra/10.jpg",
-                "https://storage.googleapis.com/proyectoprogra/11.jpg",
-                "https://storage.googleapis.com/proyectoprogra/12.jpg",
-                "https://storage.googleapis.com/proyectoprogra/13.jpg",
-                "https://storage.googleapis.com/proyectoprogra/14.jpg",
-                "https://storage.googleapis.com/proyectoprogra/15.jpg"
+                "https://storage.googleapis.com/proyectoprograv/1.jpg",
+                "https://storage.googleapis.com/proyectoprograv/2.jpg",
+                "https://storage.googleapis.com/proyectoprograv/3.jpg",
+                "https://storage.googleapis.com/proyectoprograv/4.jpg",
+                "https://storage.googleapis.com/proyectoprograv/5.jpg",
+                "https://storage.googleapis.com/proyectoprograv/6.jpg",
+                "https://storage.googleapis.com/proyectoprograv/7.jpg",
+                "https://storage.googleapis.com/proyectoprograv/8.jpg",
+                "https://storage.googleapis.com/proyectoprograv/9.jpg",
+                "https://storage.googleapis.com/proyectoprograv/10.jpg",
+                "https://storage.googleapis.com/proyectoprograv/11.jpg",
+                "https://storage.googleapis.com/proyectoprograv/12.jpg",
+                "https://storage.googleapis.com/proyectoprograv/13.jpg",
+                "https://storage.googleapis.com/proyectoprograv/14.jpg",
+                "https://storage.googleapis.com/proyectoprograv/15.jpg"
         };
     }
 }
