819d5c046b794203fa9f757f47552486104035c8
Esther2899 <estercris28@hotmail.com>
2024-04-21 19:16:47 -0600

    cambios

---
+++ ViewModels/CarritoViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.Input;
+using Login1.Models.Entidades;
+
+namespace Login1.ViewModels
+{
+    public class CarritoViewModel
+    {
+        public ICommand MostrarCarritoCommand { get; }
+
+        public CarritoViewModel()
+        {
+            MostrarCarritoCommand = new RelayCommand(MostrarCarrito);
+        }
+
+        private void MostrarCarrito()
+        {
+            // Lógica para mostrar los productos en el carrito
+            foreach (var producto in CarritoCompra.ProductosCarrito)
+            {
+                Console.WriteLine($"Producto: {producto.Name}, Precio: {producto.Precio}");
+            }
+        }
+    }
+}
--- Vistas/Carrito.xaml.cs
+++ Vistas/Carrito.xaml.cs
@@ -1,4 +1,5 @@
 using Login1.Models.Entidades;
+using Login1.ViewModels;
 
 namespace Login1.Vistas;
 
@@ -7,18 +8,20 @@ public partial class Carrito : ContentPage
 	public Carrito()
 	{
 		InitializeComponent();
-	}
+        BindingContext = new CarritoViewModel();
+    }
     private async void IrAMainPage_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new MainPage());
     }
-    private async void IrACarrito_Clicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new Carrito());
-    }
+   
     private async void IrAPerfil_Clicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new Perfil());
+    }
+    private async void IrACarrito_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Carrito());
     }
 
 }
